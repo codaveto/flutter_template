3a6426010d2d252383fb690d73da3b2f6a12d605
Brian Manuputty <brian.manuputty@wolfpackit.nl>
2022-04-24 02:18:06 +0200

    [SETUP] Add fonts, create script for directories, main packages and string files

---
+++ scripts/create_feature.sh
@@ -0,0 +1,23 @@
+cd ../lib/features/ || exit
+echo "Name of the feature?"
+read -r feature
+mkdir "$feature"
+cd "$feature" || exit
+mkdir "abstracts"
+mkdir "analytics"
+mkdir "api"
+mkdir "data"
+cd data || exit
+mkdir "constants"
+mkdir "dtos"
+mkdir "enums"
+mkdir "exceptions"
+mkdir "models"
+mkdir "requests"
+mkdir "responses"
+cd ../
+mkdir "forms"
+mkdir "services"
+mkdir "strings"
+mkdir "views"
+mkdir "widgets"
